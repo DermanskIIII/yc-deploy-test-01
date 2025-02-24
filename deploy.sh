7d5de8875adcaa6179f4ea94975d8ef16be37945
Ivan Dermanskiy <dermanskiiii@gmail.com>
2025-02-24 15:41:48 +0500

    few fixes

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ GITHUB_REPO_NAME=yc-deploy-test-01
 
 USERNAME=$(whoami)
 
-echo "Подготовка клонированию git-репозитория (\~/.ssh/known_hosts, deploy-key для доступа к репозиторию)..."
+echo "Подготовка клонированию git-репозитория (~/.ssh/known_hosts, deploy-key для доступа к репозиторию)..."
 install -m 700 -d /home/$USERNAME/.ssh
 cat >> /home/$USERNAME/.ssh/known_hosts << EOF
 github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
@@ -30,20 +30,23 @@ EOF
 chmod 600 /home/$USERNAME/.ssh/test_deploy_key
 
 echo "Установка ПО git, docker.io и их зависимостей (может выполняться несколько минут)..."
-sudo apt-get update 2>&1 >/dev/null
-sudo apt-get install -y --no-install-recommends git docker.io 2>&1 >/dev/null
+sudo apt-get update >/dev/null 2>&1
+sudo apt-get install -y --no-install-recommends git docker.io >/dev/null 2>&1
 echo "Успешно"
 
 sudo usermod -aG docker $USERNAME
 
 echo "Установка ПО Yandex.Cloud (утилита yc)..."
+curl -sSL https://storage.yandexcloud.net/yandexcloud-yc/install.sh 2>/dev/null | \
+  sudo bash -s -- -r /home/$USERNAME/.bashrc -i /opt/yc 2>/dev/null
+echo "Успешно"
 TEMP_DIR=`mktemp -dq`
 cd $TEMP_DIR
 
 OLD_GIT_SSHCOMMAND=`bash +e -c "git config --global core.sshCommand; exit 0"`
 git config --global core.sshCommand "ssh -i /home/$USERNAME/.ssh/test_deploy_key"
 echo "Клонируем репозиторий приложения..."
-git clone $GITHUB_CLONE_URL/$GITHUB_REPO_NAME.git 2>&1 >/dev/null
+git clone $GITHUB_CLONE_URL/$GITHUB_REPO_NAME.git >/dev/null 2>&1
 rm /home/$USERNAME/.ssh/test_deploy_key
 echo "Успешно"
 mv $TEMP_DIR/$GITHUB_REPO_NAME/demo.sh ~/
@@ -51,6 +54,7 @@ test -z "$OLD_GIT_SSHCOMMAND" && \
   git config --global --unset core.sshCommand || \
   git config --global core.sshCommand "$OLD_GIT_SSHCOMMAND"
 
+read -p "Введите Cloud.ID: " YC_CLOUD_ID
 read -sp "Введите OAuth токен Yandex.Cloud (введённые символы не отображаются!):" YC_OAUTH_TOKEN
 echo
 newgrp docker << EOF
@@ -59,17 +63,17 @@ newgrp docker << EOF
   /opt/yc/bin/yc config set folder-id "$YC_FOLDER_ID"
   /opt/yc/bin/yc config set token "$YC_OAUTH_TOKEN"
   echo "Создаём реестр докер-образов ..."
-  export YC_REGISTRY_ID=\`yc container registry create | grep -E "^id" | awk '{ print \$2 }'\`
+  export YC_REGISTRY_ID=\`/opt/yc/bin/yc container registry create | grep -E "^id" | awk '{ print \$2 }'\`
   /opt/yc/bin/yc container registry configure-docker
   cd $TEMP_DIR/$GITHUB_REPO_NAME/docker
-  echo "Приступаем к сборке образа..."
-  docker build -t $DOCKER_IMAGE_NAME . 2>&1 >/dev/null
+  echo "Приступаем к сборке образа (может выполняться несколько минут)..."
+  docker build -t $DOCKER_IMAGE_NAME . >/dev/null 2>&1
   echo "Успешно"
   cd ~
   rm -rf $TEMP_DIR
   docker tag $DOCKER_IMAGE_NAME cr.yandex/\$YC_REGISTRY_ID/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
-  echo "Загружаем образ в реестр..."
-  docker push cr.yandex/\$YC_REGISTRY_ID/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG 2>&1 >/dev/null
+  echo "Загружаем образ в реестр (может выполняться несколько минут)..."
+  docker push cr.yandex/\$YC_REGISTRY_ID/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG >/dev/null 2>&1
   echo "Успешно"
   docker run -d --restart always --name $CONTAINER_NAME cr.yandex/\$YC_REGISTRY_ID/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
   echo "Контейнер запущен и ожидает запроса на 80 порту."
